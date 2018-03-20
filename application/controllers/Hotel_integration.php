d51e0220773f89ceaf75de4d15ac40df260979b2
Rafael Manalo <manaloraf03@gmail.com>
2018-03-20 15:24:47 +0800

    Hotel Integration Standard
    Hotel Integration Standard Added

--- application/controllers/Hotel_integration.php
+++ application/controllers/Hotel_integration.php
@@ -13,7 +13,9 @@ class Hotel_integration extends CORE_Controller
                 'Hotel_integration_model',
                 'Users_model',
                 'Sched_expense_integration',
-                'Departments_model'
+                'Departments_model',
+                'Customers_model',
+
 
             )
 
@@ -30,14 +32,21 @@ class Hotel_integration extends CORE_Controller
         $data['title'] = 'Hotel Integration';
 
         $data['accounts'] = $this->Account_title_model->get_list(array('is_deleted'=>FALSE));
-        $current_accounts= $this->Hotel_integration_model->get_list();
-        $data['current_accounts'] =$current_accounts[0];
+        $poleng_villa_accounts= $this->Hotel_integration_model->get_list(2);
+        $poleng_apartment_accounts= $this->Hotel_integration_model->get_list(3);
+        $red_rose_accounts= $this->Hotel_integration_model->get_list(4);
+        $hayvenhurst_accounts= $this->Hotel_integration_model->get_list(5);
+        $data['poleng_villa_accounts'] =$poleng_villa_accounts[0];
+        $data['poleng_apartment_accounts'] =$poleng_apartment_accounts[0];
+        $data['red_rose_accounts'] =$red_rose_accounts[0];
+        $data['hayvenhurst_accounts'] =$hayvenhurst_accounts[0];
         $data['departments'] = $this->Departments_model->get_list(array('is_active'=>TRUE, 'is_deleted'=>FALSE));
+        $data['customers'] = $this->Customers_model->get_list(array('is_active'=>TRUE, 'is_deleted'=>FALSE));
 
 
-        (in_array('15-2',$this->session->user_rights)? 
-        $this->load->view('hotel_integration_view', $data)
-        :redirect(base_url('dashboard')));
+        // (in_array('6-13',$this->session->user_rights)? 
+        $this->load->view('hotel_integration_view', $data);
+        // :redirect(base_url('dashboard')));
         
 
     }
@@ -45,29 +54,25 @@ class Hotel_integration extends CORE_Controller
 
     public function transaction($txn=null){
         switch($txn){
-
-
             case 'save':
                 $m_integration=$this->Hotel_integration_model;
-
-                $m_integration->delete(1); //delete it first
-
-                $m_integration->hotel_integration_id=1;
-
-                //suppliers
-                $m_integration->income_rs=$this->input->post('income_rs',TRUE);
-                $m_integration->income_bs=$this->input->post('income_bs',TRUE);
-                $m_integration->income_os=$this->input->post('income_os',TRUE);
-                $m_integration->liability_apd=$this->input->post('liability_apd',TRUE);
-                $m_integration->liability_sd=$this->input->post('liability_sd',TRUE);
-                $m_integration->liability_ud=$this->input->post('liability_ud',TRUE);
-                $m_integration->asset_cash=$this->input->post('asset_cash',TRUE);
-                $m_integration->asset_check=$this->input->post('asset_check',TRUE);
-                $m_integration->asset_card=$this->input->post('asset_card',TRUE);
-                $m_integration->asset_charge=$this->input->post('asset_charge',TRUE);
-                $m_integration->withhold_tax=$this->input->post('withhold_tax',TRUE);
-                $m_integration->department_id=$this->input->post('department_id',TRUE);
-                $m_integration->save();
+                $department_id =$this->input->post('department_id'); // department id as Hotel_settings_id
+                $m_integration->room_sales_id=$this->input->post('room_sales_id',TRUE);
+                $m_integration->bar_sales_id=$this->input->post('bar_sales_id',TRUE);
+                $m_integration->other_sales_id=$this->input->post('other_sales_id',TRUE);
+                $m_integration->adv_sales_id=$this->input->post('adv_sales_id',TRUE);
+                $m_integration->adv_cash_id=$this->input->post('adv_cash_id',TRUE);
+                $m_integration->adv_check_id=$this->input->post('adv_check_id',TRUE);
+                $m_integration->adv_card_id=$this->input->post('adv_card_id',TRUE);
+                $m_integration->adv_charge_id=$this->input->post('adv_charge_id',TRUE);
+                $m_integration->adv_bank_id=$this->input->post('adv_bank_id',TRUE);
+                $m_integration->cash_id=$this->input->post('cash_id',TRUE);
+                $m_integration->check_id=$this->input->post('check_id',TRUE);
+                $m_integration->card_id=$this->input->post('card_id',TRUE);
+                $m_integration->charge_id=$this->input->post('charge_id',TRUE);
+                $m_integration->bank_id=$this->input->post('bank_id',TRUE);
+                $m_integration->customer_id=$this->input->post('customer_id',TRUE);
+                $m_integration->modify($department_id);
 
                 $response['stat']="success";
                 $response['title']="Success!";
@@ -77,37 +82,6 @@ class Hotel_integration extends CORE_Controller
 
                 break;
 
-
-         
         }
-
-
-
-
-
     }
-
-
-    function get_response_rows($filter){
-        $m_acc_period=$this->Accounting_period_model;
-
-        return $m_acc_period->get_list(
-            $filter,
-            array(
-                'CONCAT(DATE_FORMAT(period_start,"%M %d, %Y")," to ",DATE_FORMAT(period_end,"%M %d, %Y")) as date_covered',
-                'accounting_period.*',
-                'CONCAT_WS(" ",ua.user_fname,ua.user_lname) as user'
-
-            ),
-            array(
-                array('user_accounts as ua','ua.user_id=accounting_period.closed_by_user','left')
-            ),
-
-            'accounting_period.accounting_period_id DESC'
-        );
-    }
-
-
-
-
 }
