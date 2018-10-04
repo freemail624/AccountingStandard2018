de4e258c175f118c9d967c8a120c24705fff9bd9
Unknown <manaloraf03@gmail.com>
2018-10-04 13:32:39 +0800

    General Journal Bug on Spinning

--- application/models/Rights_link_model.php
+++ application/models/Rights_link_model.php
@@ -97,7 +97,9 @@ class Rights_link_model extends CORE_Model{
                                           (87,'3','3-5','Warehouse Dispatching'),
                                           (88,'4','4-7','Brands'),
                                           (89,'16','16-1','Monthly Percentage Tax Return'),
-                                          (90,'16','16-2','Quarterly Percentage Tax Return')
+                                          (90,'16','16-2','Quarterly Percentage Tax Return'),
+                                          (91,'16','16-3','Certificate of Creditable Tax')
+
                                           ON DUPLICATE KEY UPDATE
                                           rights_links.parent_code=VALUES(rights_links.parent_code),
                                           rights_links.link_code=VALUES(rights_links.link_code),
