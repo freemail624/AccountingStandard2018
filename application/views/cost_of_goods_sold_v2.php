fba09d2adbb83c922af3535bf1ff236811b71a9a
Unknown <manaloraf03@gmail.com>
2018-10-30 09:45:57 +0800

    AS - Minor Revision

--- application/views/cost_of_goods_sold_v2.php
+++ application/views/cost_of_goods_sold_v2.php
@@ -106,7 +106,7 @@
                                                 <div class="col-lg-3">
                                                         From :<br />
                                                         <div class="input-group">
-                                                            <input type="text" id="txt_start_date" name="" class="date-picker form-control" value="<?php echo date("m").'/01/'.date("Y"); ?>">
+                                                            <input type="text" id="txt_start_date" name="" class="date-picker form-control" value="01/01/<?php echo date("Y"); ?>">
                                                              <span class="input-group-addon">
                                                                     <i class="fa fa-calendar"></i>
                                                              </span>
