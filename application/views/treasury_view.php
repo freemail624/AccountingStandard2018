3630a9ea435ff1ff1d97cdd93106089031ae5869
Unknown <manaloraf03@gmail.com>
2018-12-20 09:59:53 +0800

    MM - Revision of Footer

--- application/views/treasury_view.php
+++ application/views/treasury_view.php
@@ -551,7 +551,7 @@ button[name="mark_delivered"]{
 <footer role="contentinfo">
     <div class="clearfix">
         <ul class="list-unstyled list-inline pull-left">
-            <li><h6 style="margin: 0;">&copy; 2016 - Paul Christian Rueda</h6></li>
+            <li><h6 style="margin: 0;">&copy; 2018 - JDEV OFFICE SOLUTION INC</h6></li>
         </ul>
         <button class="pull-right btn btn-link btn-xs hidden-print" id="back-to-top"><i class="ti ti-arrow-up"></i></button>
     </div>
