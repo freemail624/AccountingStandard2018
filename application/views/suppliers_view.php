3d57bd0edb53c7788ab44fbdff820030e4103b6f
RAFAEL MANALO <manaloraf03@gmail.com>
2019-03-27 11:08:12 +0800

    Supplier Masterlist Print and Export

--- application/views/suppliers_view.php
+++ application/views/suppliers_view.php
@@ -97,11 +97,7 @@ $(document).ready(function(){
                     }
                 ]
             });
-            var createToolBarButton=function() {
-                var _btnNew='<button class="btn btn-primary"  id="btn_new" style="text-transform: capitalize;font-family: Tahoma, Georgia, Serif;" data-toggle="modal" data-target="" data-placement="left" title="New supplier" >'+
-                    '<i class="fa fa-plus"></i> New Supplier</button>';
-                $("div.toolbar").html(_btnNew);
-            }();
+
         }();
 
         $('.numeric').autoNumeric('init');
@@ -167,6 +163,11 @@ $(document).ready(function(){
             $('img[name="img_user"]').attr('src','assets/img/anonymous-icon.png');
         });
 
+        $("#searchbox_suppliers").keyup(function(){         
+            dt
+                .search(this.value)
+                .draw();
+        });
         $('#tbl_suppliers tbody').on('click','button[name="edit_info"]',function(){
             _txnMode="edit";
             $('#modal_create_suppliers').modal('show');
@@ -239,6 +240,14 @@ $(document).ready(function(){
         $('#btn_cancel').click(function(){
             showList(true);
         });
+
+        $('#btn_print').click(function(){
+           window.open('suppliers/transaction/print-masterfile');
+        });  
+
+        $('#btn_export').click(function(){
+           window.open('suppliers/transaction/export-supplier');
+        });                
 
         $('#btn_save').click(function(){
             if(validateRequiredFields()){
@@ -516,7 +525,9 @@ $(document).ready(function(){
         #img_user {
             padding-bottom: 15px;
         }
-
+        #tbl_suppliers_filter{
+            display: none;
+        }
     </style>
 
 </head>
@@ -546,11 +557,24 @@ $(document).ready(function(){
 
                                     <div id="div_supplier_list">
                                         <div class="panel panel-default">
-<!--                                             <div class="panel-heading">
-                                                <b style="color: white; font-size: 12pt;"><i class="fa fa-bars"></i>&nbsp; Suppliers</b>
-                                            </div>
- -->                                            <div class="panel-body table-responsive">
+
+
+                                                  <div class="panel-body table-responsive">
                                             <h2 class="h2-panel-heading">Suppliers</h2><hr>
+                                            <div class="row">
+                                                <div class="col-lg-3"><br>
+                                                        <button class="btn btn-primary"  id="btn_new" style="text-transform: capitalize;font-family: Tahoma, Georgia, Serif;" data-toggle="modal" data-target="" data-placement="left" title="New supplier" ><i class="fa fa-plus"></i> New Supplier</button>
+                                                </div>
+                                                <div class="col-lg-offset-3 col-lg-3" style="text-align: right;">
+                                                &nbsp;<br>
+                                                        <button class="btn btn-primary" id="btn_print" style="text-transform: none; font-family: Tahoma, Georgia, Serif;padding: 6px 10px!important;" data-toggle="modal" data-target="#salesInvoice" data-placement="left" title="Print Supplier Masterfile" ><i class="fa fa-print"></i> Print</button> &nbsp;
+                                                        <button class="btn btn-success" id="btn_export" style="text-transform: none; font-family: Tahoma, Georgia, Serif;padding: 6px 10px!important;" data-toggle="modal" data-target="#salesInvoice" data-placement="left" title="Export Supplier Masterfile" ><i class="fa fa-file-excel-o"></i> Export</button>
+                                                </div>
+                                                <div class="col-lg-3">
+                                                        Search :<br />
+                                                         <input type="text" id="searchbox_suppliers" class="form-control">
+                                                </div>
+                                            </div><br>
                                                 <table id="tbl_suppliers" class="table table-striped" cellspacing="0" width="100%">
                                                     <thead class="">
                                                     <tr>
