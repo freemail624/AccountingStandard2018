9ee933696a205bbe532a3a24317761b648a97dee
Joash Noble <noblejjoash@gmail.com>
2021-01-18 12:10:54 +0800

    Minor revision of T-Accounts (Summary Reports)

--- application/views/template/book_of_accounts_report_summary.php
+++ application/views/template/book_of_accounts_report_summary.php
@@ -18,7 +18,10 @@
 		@media print {
       @page { margin: 0; }
       body { margin: 1.0cm; }
+
 }
+
+@media print{@page {size: landscape}}
 	</style>
 	<script>
 		(function(){
@@ -39,7 +42,7 @@
         </tr>
     </table><hr>
     <div>
-        <h3><strong>T-ACCOUNT (<?php echo $title; ?>)<br> Date: <u><?php echo $_GET['s']; ?></u> to <u><?php echo $_GET['e']; ?></u><br>
+        <h3><strong>T-ACCOUNT (<?php echo $title; ?>) <br> Date: <u><?php echo $_GET['s']; ?></u> to <u><?php echo $_GET['e']; ?></u><br>
         <?php if($_GET['d'] == 0){?> 
         All Departments
         <?php } else {?>
@@ -50,8 +53,12 @@
     </div>
     <table width="100%" border="1" cellspacing="0">
 	    <thead>
-		    <th align="left">Account No</th>
-		    <th align="left">Account Title</th>
+		    <th align="left" width="15%">Txn #</th>
+		    <th align="left">Date</th>
+		    <th align="left">Particular</th>
+		    <th align="left">Reference</th>
+		    <th align="left" width="25%">Remarks</th>
+		    <th align="left">Account</th>
 		    <th align="right">Dr</th>
 		    <th align="right">Cr</th>
 	    </thead>
@@ -60,7 +67,11 @@
 	    <?php $sum_cr=0; ?>
 	    <?php foreach($journal_list as $journal) { ?>
 	    	<tr>
-	    		<td style="padding: 5px;"><?php echo $journal->account_no; ?></td>
+	    		<td style="padding: 5px;"><?php echo $journal->txn_no; ?></td>
+	    		<td style="padding: 5px;"><?php echo $journal->date_txn; ?></td>
+	    		<td style="padding: 5px;"><?php echo $journal->description; ?></td>
+	    		<td style="padding: 5px;"><?php echo $journal->reference_desc; ?></td>
+	    		<td style="padding: 5px;"><?php echo $journal->remarks; ?></td>
 	    		<td style="padding: 5px;"><?php echo $journal->account_title; ?></td>
 	    		<td style="padding: 5px; text-align: right;"><?php echo number_format($journal->dr_amount,2); ?></td>
 	    		<td style="padding: 5px; text-align: right;"><?php echo number_format($journal->cr_amount,2); ?></td>
@@ -69,7 +80,7 @@
 	    	</tr>
 		<?php } ?>
 			<tr>
-				<td colspan="2" align="right">Total:</td>
+				<td colspan="6" align="right">Total:</td>
 				<td style="padding: 5px;" align="right"><strong><?php echo number_format($sum_dr,2); ?></strong></td>
 				<td style="padding: 5px;" align="right"><strong><?php echo number_format($sum_cr,2); ?></strong></td>
 			</tr>
